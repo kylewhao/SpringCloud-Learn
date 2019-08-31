e0c74aa66a68f5245795848053e3a48d296b03ff
kylewh <411321707@qq.com>
2019-08-31 14:21:56 +0800

    修改

--- mycloud-eureka-client/src/main/java/com/kylewh/mycloudeurekaclient/service/impl/WorkerServiceImpl.java
+++ mycloud-eureka-client/src/main/java/com/kylewh/mycloudeurekaclient/service/impl/WorkerServiceImpl.java
@@ -1,4 +1,18 @@
 package com.kylewh.mycloudeurekaclient.service.impl;
 
-public class WorkerServiceImpl {
+import com.baomidou.dynamic.datasource.annotation.DS;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.kylewh.mycloudapi.entity.Worker;
+import com.kylewh.mycloudeurekaclient.constant.DataSourceConstant;
+import com.kylewh.mycloudeurekaclient.mapper.WorkerMapper;
+import com.kylewh.mycloudeurekaclient.service.WorkerService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author kylewhao
+ */
+@Service
+@DS(DataSourceConstant.DS_MASTER)
+public class WorkerServiceImpl extends ServiceImpl<WorkerMapper, Worker> implements WorkerService {
+
 }
